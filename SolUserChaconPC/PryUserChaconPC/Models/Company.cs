6f5cc5b758fa7ba4fa02bf47d4817e04688a2f80
Pedro <a2019114887@estudiantes.edu.bo>
2021-01-25 19:59:24 -0400

    webconfig y DataContext realizados

--- SolUserChaconPC/PryUserChaconPC/Models/Company.cs
+++ SolUserChaconPC/PryUserChaconPC/Models/Company.cs
@@ -6,6 +6,6 @@
         [Key]
         public string name { get; set; }
         public string catchPhrase { get; set; }
-        public string bs { get; set; }
+       
     }
 }
---
+++ SolUserChaconPC/PryUserChaconPC/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PryUserChaconPC.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
