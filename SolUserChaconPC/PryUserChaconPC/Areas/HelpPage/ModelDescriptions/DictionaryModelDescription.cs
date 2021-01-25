0c1372e6f7c80c5de92ab864f8f95854dcc6fbd1
Pedro <a2019114887@estudiantes.edu.bo>
2021-01-25 19:22:22 -0400

    Iniciando Proyecto

---
+++ SolUserChaconPC/PryUserChaconPC/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PryUserChaconPC.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SolUserChaconPC/PryUserChaconPC/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PryUserChaconPC.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
