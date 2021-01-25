0b44a1f959b08cd6ac7e2514c2f453fab2484648
Pedro <a2019114887@estudiantes.edu.bo>
2021-01-25 19:30:04 -0400

    Clases creadas

---
+++ SolUserChaconPC/PryUserChaconPC/Models/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PryUserChaconPC.Models
+{
+    public class Company
+    {
+        public string name { get; set; }
+        public string catchPhrase { get; set; }
+        public string bs { get; set; }
+    }
+}
---
+++ SolUserChaconPC/PryUserChaconPC/Models/Geo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PryUserChaconPC.Models
+{
+    public class Geo
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+    }
+}
